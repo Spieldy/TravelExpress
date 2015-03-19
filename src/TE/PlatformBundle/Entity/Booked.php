6a5524b7afd6884906c6e10ca92c89df95223c7e
Spieldy <spieldennerjeremy@gmail.com>
2015-03-19 17:28:31 -0400

    add database table for lift and booked

---
+++ src/TE/PlatformBundle/Entity/Booked.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace TE\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Booked
+ *
+ * @ORM\Table(name="booked")
+ * @ORM\Entity(repositoryClass="TE\PlatformBundle\Entity\BookedRepository")
+ */
+class Booked
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+    * @ORM\ManyToMany(targetEntity="TE\UserBundle\Entity\User", cascade={"persist"})
+    */
+    private $users;
+
+    /**
+    * @ORM\ManyToOne(targetEntity="TE\PlatformBundle\Entity\Lift")
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $lift;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="seats", type="integer")
+     */
+    private $seats;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    // Notez le singulier, on ajoute une seule catégorie à la fois
+    public function addUser(User $user)
+    {
+      // Ici, on utilise l'ArrayCollection vraiment comme un tableau
+      $this->users[] = $user;
+
+      return $this;
+    }
+
+    public function removeUser(User $user)
+    {
+      // Ici on utilise une méthode de l'ArrayCollection, pour supprimer la catégorie en argument
+      $this->users->removeElement($user);
+    }
+
+    // Notez le pluriel, on récupère une liste de catégories ici !
+    public function getUsers()
+    {
+      return $this->users;
+    }
+
+    public function setLift(Lift $lift)
+    {
+      $this->lift = $lift;
+
+      return $this;
+    }
+
+    public function getLift()
+    {
+      return $this->lift;
+    }
+
+    /**
+     * Set seats
+     *
+     * @param integer $seats
+     * @return Booked
+     */
+    public function setSeats($seats)
+    {
+        $this->seats = $seats;
+
+        return $this;
+    }
+
+    /**
+     * Get seats
+     *
+     * @return integer
+     */
+    public function getSeats()
+    {
+        return $this->seats;
+    }
+}
---
+++ src/TE/PlatformBundle/Entity/Lift.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace TE\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Lift
+ *
+ * @ORM\Table(name="lift")
+ * @ORM\Entity(repositoryClass="TE\PlatformBundle\Entity\LiftRepository")
+ */
+class Lift
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="TE\UserBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fromCity", type="string", length=255)
+     */
+    private $fromCity;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="toCity", type="string", length=255)
+     */
+    private $toCity;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateLift", type="datetime")
+     */
+    private $dateLift;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal")
+     */
+    private $price;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="isAvailable", type="boolean")
+     */
+    private $isAvailable;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setUser(User $user)
+    {
+      $this->user = $user;
+
+      return $this;
+    }
+
+    public function getUser()
+    {
+      return $this->user;
+    }
+
+    /**
+     * Set fromCity
+     *
+     * @param string $fromCity
+     * @return Lift
+     */
+    public function setFromCity($fromCity)
+    {
+        $this->fromCity = $fromCity;
+
+        return $this;
+    }
+
+    /**
+     * Get fromCity
+     *
+     * @return string
+     */
+    public function getFromCity()
+    {
+        return $this->fromCity;
+    }
+
+    /**
+     * Set toCity
+     *
+     * @param string $toCity
+     * @return Lift
+     */
+    public function setToCity($toCity)
+    {
+        $this->toCity = $toCity;
+
+        return $this;
+    }
+
+    /**
+     * Get toCity
+     *
+     * @return string
+     */
+    public function getToCity()
+    {
+        return $this->toCity;
+    }
+
+    /**
+     * Set dateLift
+     *
+     * @param \DateTime $dateLift
+     * @return Lift
+     */
+    public function setDateLift($dateLift)
+    {
+        $this->dateLift = $dateLift;
+
+        return $this;
+    }
+
+    /**
+     * Get dateLift
+     *
+     * @return \DateTime
+     */
+    public function getDateLift()
+    {
+        return $this->dateLift;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     * @return Lift
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set isAvailable
+     *
+     * @param boolean $isAvailable
+     * @return Lift
+     */
+    public function setIsAvailable($isAvailable)
+    {
+        $this->isAvailable = $isAvailable;
+
+        return $this;
+    }
+
+    /**
+     * Get isAvailable
+     *
+     * @return boolean
+     */
+    public function getIsAvailable()
+    {
+        return $this->isAvailable;
+    }
+}
